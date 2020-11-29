2af1a3e960964ba642d3afb6d828cd2292d3a7c9
steph <stephanie.bulela@gmail.com>
2020-11-29 20:22:37 +0100

    Poprawa obliczania średniego czasu przebywania w systemie i średniego czasu obsługi

--- mm1_sim.py
+++ mm1_sim.py
@@ -21,6 +21,7 @@ import stdrandom
 from linkedqueue import Queue
 from statistics import mean
 
+# Pobieranie danych
 arrv_rate = float(sys.argv[1])
 srv_time = float(sys.argv[2])
 limit = int(sys.argv[3])
@@ -43,33 +44,44 @@ while next_arrival < limit:
 
     while next_arrival < next_service:
         queue.enqueue(next_arrival)
-        next_arrival += stdrandom.exp(srv_time)
+        next_arrival += stdrandom.exp(arrv_rate)
 
+#obsługa nowego pakietu
     arrival = queue.dequeue()
+#czas przebywania w systemie = czas kiedy klient zostanie obsłużony - czas przybycia klienta
     wait_time = next_service - arrival
 
+#czas obsługi w serwerze = cały czas w systemie - czas wyjścia poprzedniego klienta
     wait_duration = wait_time - prev_wait_time
     prev_wait_time = wait_time
 
     if queue.isEmpty():
         prev_service_time = next_service
         next_service = next_arrival + stdrandom.exp(srv_time)
-        service_duration = next_service - prev_service_time
+#jak kolejka jest pusta to czas trawnia obsługi = czas wyjścia - czas przyjścia
+        service_duration = next_service - next_arrival
     else:
         prev_service_time = next_service
         next_service = next_service + stdrandom.exp(srv_time)
+# jak kolejka nie jest pusta to czas trawnia obsługi = czas wyjścia - czas wyjścia poprzedniego klienta
         service_duration = next_service - prev_service_time
 
-    wait_list.append(wait_duration)
+#średni czas przebywania w systemie
+    wait_list.append(wait_time)
+#średni czas obsługi
     service_list.append(service_duration)
 
 mean_wait = mean(wait_list)
 mean_service = mean(service_list)
-mean_system = mean_wait + mean_service
+
+#zakomentowalam i moim zdaniem to jest do usunięcia to bo my tutaj nigdzie nie obliczamy średniego czasu przebywania w kolejce
+#mean_system = mean_wait + mean_service
 
 print()
-print("Mean simulation queue wait time: {}".format(mean_wait))
-print("Mean simulation service time: {}".format(mean_service))
-print("Mean simulation system time: {}".format(mean_system))
+#print("Mean simulation queue wait time: {}".format(mean_wait))
+print("Mean simulation service time(średni czas obsługi): {}".format(mean_service))
+print("Mean simulation system time (średni czas przebywania w systemie): {}".format(mean_wait))
 
-print("Mean theoretical system time: {}".format(mean_system_theo))
+print("Mean theoretical system time (teoretyczny średni czas obsługi): {}".format(1/srv_time))
+print("Mean theoretical system time (teoretyczny średni czas przebywania w systemie): {}".format(mean_system_theo))
+
